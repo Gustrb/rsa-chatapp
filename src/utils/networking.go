af7dd24f6947d2096a7bd7139fd0ecc90ac20c8c
gustrb <gustavoreisbauer@gmail.com>
2024-10-13 11:47:54 -0300

    chore: allocating both functions in one file

--- src/utils/networking.go
+++ src/utils/networking.go
@@ -25,3 +25,16 @@ func ReceiveWithLengthPrefix(conn net.Conn) ([]byte, error) {
 	}
 	return data, nil
 }
+
+// sendWithLengthPrefix sends data prefixed with its length as 4 bytes
+func SendWithLengthPrefix(conn net.Conn, data []byte) error {
+	length := len(data)
+	lengthBytes := []byte{
+		byte((length >> 24) & 0xFF),
+		byte((length >> 16) & 0xFF),
+		byte((length >> 8) & 0xFF),
+		byte(length & 0xFF),
+	}
+	_, err := conn.Write(append(lengthBytes, data...))
+	return err
+}
